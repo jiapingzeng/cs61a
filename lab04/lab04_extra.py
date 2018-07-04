595f5afefd362c86b90e848b7bf0ee3d0e97eb02
Jiaping Zeng <jiaping@gisar.me>
2018-07-03 21:18:34 -0700

    progress on stuff

---
+++ lab04/lab04_extra.py
@@ -0,0 +1,90 @@
+""" Lab 04 Optional Questions """
+
+from lab04 import *
+
+# Q5
+def map(fn, lst):
+    """Maps fn onto lst using mutation.
+    >>> original_list = [5, -1, 2, 0]
+    >>> map(lambda x: x * x, original_list)
+    >>> original_list
+    [25, 1, 4, 0]
+    """
+    for i in range(0, len(lst)):
+        lst[i] = fn(lst[i])
+
+# Q6
+def pascal(row, column):
+    """Returns a number corresponding to the value at that location
+    in Pascal's Triangle.
+    >>> pascal(0, 0)
+    1
+    >>> pascal(0, 5)	# Empty entry; outside of Pascal's Triangle
+    0
+    >>> pascal(3, 2)	# Row 4 (1 3 3 1), 3rd entry
+    3
+    """
+    if column < 0 or column > row: return 0
+    elif column == 0 or column == row: return 1
+    else: return pascal(row-1, column-1) + pascal(row-1, column)
+
+# Q7
+def insert_into_all(item, nested_list):
+    """Assuming that nested_list is a list of lists, return a new list
+    consisting of all the lists in nested_list, but with item added to
+    the front of each.
+
+    >>> nl = [[], [1, 2], [3]]
+    >>> insert_into_all(0, nl)
+    [[0], [0, 1, 2], [0, 3]]
+    """
+    for lst in nested_list:
+        lst.insert(0, item)
+
+def subseqs(s):
+    """Assuming that S is a list, return a nested list of all subsequences
+    of S (a list of lists). The subsequences can appear in any order.
+
+    >>> seqs = subseqs([1, 2, 3])
+    >>> sorted(seqs)
+    [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]
+    >>> subseqs([])
+    [[]]
+    """
+    r = [[]]
+    for i in s:
+        new = []
+        for j in r:
+            new.append(j + [i])
+        r += new
+    return r
+
+# Q8
+def merge(lst1, lst2):
+    """Merges two sorted lists.
+
+    >>> merge([1, 3, 5], [2, 4, 6])
+    [1, 2, 3, 4, 5, 6]
+    >>> merge([], [2, 4, 6])
+    [2, 4, 6]
+    >>> merge([1, 2, 3], [])
+    [1, 2, 3]
+    >>> merge([5, 7], [2, 4, 6])
+    [2, 4, 5, 6, 7]
+    """
+    return sorted(lst1 + lst2)
+
+# Q9
+def mergesort(seq):
+    """Mergesort algorithm.
+
+    >>> mergesort([4, 2, 5, 2, 1])
+    [1, 2, 2, 4, 5]
+    >>> mergesort([])     # sorting an empty list
+    []
+    >>> mergesort([1])   # sorting a one-element list
+    [1]
+    """
+    if seq == [] or len(seq) == 1: return seq
+    half = len(seq) // 2
+    return merge(mergesort(seq[:half]), mergesort(seq[half:]))
